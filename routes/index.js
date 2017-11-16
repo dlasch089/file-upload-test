bbc94813ec424adeb34b54014c3bebe9b3f763d8
Dominik Lasch <dl@exutec.de>
2017-11-16 11:02:13 +0100

    done the fileupload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Picture = require("../models/pictures");
+
+// FOR FILEUPLOADS
+const multer = require("multer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// Route to upload from project base path
+const upload = multer({ dest: "./public/uploads/" });
+
+router.post("/upload", upload.single("photo"), (req, res, next) => {
+  const pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname
+  });
+
+  pic.save(err => {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
